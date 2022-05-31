ab4c5bb53b1c1133a0a2e8a7b9a580874ab94fab
Vladislav Zhujkov <velocrosss@yandex.ru>
2022-05-31 13:40:05 +0300

    fix settings.py

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -8,4 +8,7 @@ REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 ELASTIC_HOST = os.getenv('ES_HOST', '127.0.0.1')
 ELASTIC_PORT = int(os.getenv('ES_PORT', 9200))
 
-
+# URL приложения
+SERVICE_URL = 'http://127.0.0.1:8000'
+# Версия API
+API = '/api/v1'
