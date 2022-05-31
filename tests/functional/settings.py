f2ffd75c11d43c5a0f20826ebae779271519da9f
vladislavzujkov <velocrosss@gmail.com>
2022-05-31 22:13:43 +0300

    docker

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,12 +1,12 @@
 import os
 
 # Настройки Redis
-REDIS_HOST = os.getenv('REDIS_HOST', 'redis-cache_test')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+REDIS_HOST = os.getenv('REDIS_HOST_TEST', 'redis-cache_test')
+REDIS_PORT = int(os.getenv('REDIS_PORT_TEST', 6379))
 
 # Настройки Elasticsearch
-ELASTIC_HOST = os.getenv('ES_HOST', 'elasticsearch_test')
-ELASTIC_PORT = int(os.getenv('ES_PORT', 9200))
+ELASTIC_HOST = os.getenv('ES_HOST_TEST', 'elasticsearch_test')
+ELASTIC_PORT = int(os.getenv('ES_PORT_TEST', 9200))
 
 # URL приложения
 SERVICE_URL = os.getenv('SERVICE_URL', 'http://web_test:8000')
