f7d9f5fc2f5bc4097e071e8e64064a8b99875761
Vladislav Zhujkov <velocrosss@yandex.ru>
2022-06-06 22:18:03 +0300

    fix

--- fastapi-solution/src/services/service.py
+++ fastapi-solution/src/services/service.py
@@ -13,7 +13,3 @@ class ABSService(ABC):
     @abstractmethod
     def get_list(self, **kwargs):
         pass
-
-class Paginator(BaseModel):
-    page_size: int = Query(default=10, alias='page[size]')
-    page_number: int = Query(default=0, alias='page[number]')
